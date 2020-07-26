dc98020bc0bbd56dc80dbbdd8f0c3d015f2d2343
Hongyi Chen <hongyic3@illinois.edu>
2020-07-25 20:44:07 -0600

    sql test

--- nba_app/views.py
+++ nba_app/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.db import connection
+
+f = open("./sql_Test")
+sqlcode = f.read()
+f.close()
+cursor = connection.cursor()
+cursor.execute(sqlcode)
 
 # Create your views here.
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Hello, world. You're at the CS411 project.")
+    cur = connection.cursor()
+    cur.execute("SELECT * from Team");
+    dat = cur.fetchall()
+    return HttpResponse("Hello, world. You're at the CS411 project.<br>"+"team data: "+str(dat))
