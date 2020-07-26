f124856c558ee6f5b232054e1a445cd11cff3437
Hongyi Chen <hongyic3@illinois.edu>
2020-07-26 01:49:38 -0600

    CRUD backend

--- nba_app/urls.py
+++ nba_app/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('randominsert', views.randominsert, name='randominsert'),
-    path('randomdelete', views.randomdelete, name='randomdelete')
+    path('randomdelete', views.randomdelete, name='randomdelete'),
+    path('player_insert', views.player_insert, name='player_insert'),
+    path('player_update', views.player_update, name='player_update'),
+    path('player_select', views.player_select, name='player_select'),
+    path('player_delete', views.player_delete, name='player_delete')
 ]
--- nba_app/views.py
+++ nba_app/views.py
@@ -16,7 +16,6 @@ def randominsert(request):
     return HttpResponse("Added player %s with id %d" % (newplayer,newid))
 
 def randomdelete(request):
-    print(request.__dict__["path_info"])
     cur = connection.cursor()
     cur.execute("SELECT * FROM nba_app_Player WHERE playername LIKE 'RandomPlayer%';")
     rands = ', '.join([a[1] for a in cur.fetchall()])
@@ -24,3 +23,56 @@ def randomdelete(request):
         rands = "(none)"
     cur.execute("DELETE FROM nba_app_Player WHERE playerName LIKE 'RandomPlayer%';")
     return HttpResponse("Deleted random players: %s" % rands)
+
+"""
+/player_insert?id=ID&name=NAME
+"""
+def player_insert(request):
+    print("PLAYER INSERT")
+    pid = request.GET.get('id')
+    pname = request.GET.get('name')
+    if pid==None or pname==None:
+        return HttpResponse("Bad request")
+    pid = int(pid)
+    cur = connection.cursor()
+    cur.execute("SELECT * FROM nba_app_Player WHERE player_id=%d" % pid)
+    if len(cur.fetchall()):
+        return HttpResponse("key already exists...")
+    cur.execute("INSERT INTO nba_app_Player VALUES (%d,'%s')" % (pid,pname))
+    return HttpResponse("Inserted!")
+
+def player_update(request):
+    print("PLAYER UPDATE")
+    oldid = int(request.GET.get('oldid'))
+    if oldid==None:
+        return HttpResponse("Bad request")
+    cur = connection.cursor()
+    cur.execute("SELECT * FROM nba_app_Player WHERE player_id=%d" % oldid)
+    t = cur.fetchone()
+    if not t:
+        return HttpResponse("Player not found...")
+    _,oldname = t
+    pid = int(request.GET.get('id',oldid))
+    pname = request.GET.get('name',oldname)
+    cur.execute("UPDATE nba_app_Player SET player_id=%d,playername='%s' where player_id=%d" % (pid,pname,oldid))
+    return HttpResponse("Updated!")
+
+def player_select(request):
+    print("PLAYER SELECT")
+    cur = connection.cursor()
+    cur.execute("SELECT * FROM nba_app_Player")
+    return HttpResponse(str(cur.fetchall()))
+
+def player_delete(request):
+    print("PLAYER DELETE")
+    print("DELETE")
+    pid = request.GET.get('id')
+    if pid==None:
+        return HttpResponse("Bad request")
+    pid = int(pid)
+    cur = connection.cursor()
+    cur.execute("SELECT * FROM nba_app_Player WHERE player_id=%d" % pid)
+    if not len(cur.fetchall()):
+        return HttpResponse("Player not found...")
+    cur.execute("DELETE FROM nba_app_Player WHERE player_id=%d" % pid)
+    return HttpResponse("Deleted!")
