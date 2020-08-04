76c222bd299a7356585c5d48b5e4f2714c906f5a
GiovanniRodriguez <grodri41@illinois.edu>
2020-08-03 19:53:23 -0500

    search bar was added

--- nba_app/views.py
+++ nba_app/views.py
@@ -1,15 +1,27 @@
-from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.conf import settings
+from django.db.models import Count
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import get_object_or_404, render, redirect
 from django.db import connection
 from .forms import PlayerForm
 import random
 from .models import Player
-
 # Create your views here.
-
-
 #front end functions below
 def player_list(request):
-    context = {'player_list': Player.objects.all()} #replace with raw query in final form
+    players = Player.objects.all()
+    search_term = ''
+    if 'search' in request.GET:
+        search_term = request.GET['search']
+        players = players.filter(playername__icontains=search_term)
+    paginator = Paginator(players, 10)
+    page = request.GET.get('page')
+    players = paginator.get_page(page)
+    get_dict_copy = request.GET.copy()
+    params = get_dict_copy.pop('page', True) and get_dict_copy.urlencode()
+    context = {'player_list': players, 'search_term': search_term} #replace with raw query in final form
     return render(request,"nba_app/player_list.html",context)
 
 def player_form(request, id=0):
@@ -19,7 +31,6 @@ def player_form(request, id=0):
         else:
             player = Player.objects.get(pk = id)
             form = PlayerForm(instance = player)
-
         return render(request,"nba_app/player_form.html",{'form':form})
     else:
         if id == 0:
@@ -27,26 +38,20 @@ def player_form(request, id=0):
         else:
             player = Player.objects.get(pk = id)
             form = PlayerForm(request.POST, instance = player)
-
         if form.is_valid():
             form.save()
         return redirect("/nba_app/list")
-
-
 def delete_from_list(request, id ):
     player = Player.objects.get(pk = id)
     player.delete()
-    return redirect("/nba_app/list") 
-
-
+    return redirect("/nba_app/list")
 from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the CS411 project.")
-
-
-
+def list_overview(request):
+    players = Player.objects.all()
+    return render(request, 'overviews/overview.html', {'players' : players, 'search_term': search_term })
 #back end stuff below
-
 def randominsert(request):
     newplayer = "RandomPlayer"+str(random.randint(1,999))
     cur = connection.cursor()
@@ -54,7 +59,6 @@ def randominsert(request):
     newid = cur.fetchone()[0]+1
     cur.execute("INSERT INTO nba_app_Player VALUES (%d,'%s');" % (newid,newplayer))
     return HttpResponse("Added player %s with id %d" % (newplayer,newid))
-
 def randomdelete(request):
     cur = connection.cursor()
     cur.execute("SELECT * FROM nba_app_Player WHERE playername LIKE 'RandomPlayer%';")
@@ -63,7 +67,6 @@ def randomdelete(request):
         rands = "(none)"
     cur.execute("DELETE FROM nba_app_Player WHERE playerName LIKE 'RandomPlayer%';")
     return HttpResponse("Deleted random players: %s" % rands)
-
 """
 /player_insert?id=ID&name=NAME
 """
@@ -80,7 +83,6 @@ def player_insert(request):
         return HttpResponse("key already exists...")
     cur.execute("INSERT INTO nba_app_Player VALUES (%d,'%s')" % (pid,pname))
     return HttpResponse("Inserted!")
-
 def player_update(request):
     print("PLAYER UPDATE")
     oldid = int(request.GET.get('oldid'))
@@ -96,13 +98,11 @@ def player_update(request):
     pname = request.GET.get('name',oldname)
     cur.execute("UPDATE nba_app_Player SET player_id=%d,playername='%s' where player_id=%d" % (pid,pname,oldid))
     return HttpResponse("Updated!")
-
 def player_select(request):
     print("PLAYER SELECT")
     cur = connection.cursor()
     cur.execute("SELECT * FROM nba_app_Player")
     return HttpResponse(str(cur.fetchall()))
-
 def player_delete(request):
     print("PLAYER DELETE")
     print("DELETE")
